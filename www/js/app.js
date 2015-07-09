b418ae30f31f01f82630bd3f5d72992ef969479e
Meg Cunningham <meg_cunningham@live.com>
2015-07-09 12:02:18 -0500

    timer that does not work

--- www/js/app.js
+++ www/js/app.js
@@ -24,4 +24,36 @@ angular.module('starter', ['ionic'])
     minutes: 0,
     seconds: 0
   }
-})
+
+  $scope.countdown = {};
+  $scope.timerStart = function() {
+    $scope.countdown = $scope.time; 
+    $scope.clear = setInterval($scope.counter, 1000)
+  }
+
+  $scope.counter = function() {
+      if ($scope.countdown.seconds === 0) {
+        if ($scope.countdown.minutes === 0) {
+          $scope.countdown.hours -= 1; 
+          $scope.countdown.minutes = 59;
+        } else {
+          $scope.countdown.minutes -= 1;
+        }
+        $scope.countdown.seconds = 59;
+      } else {
+        $scope.countdown.seconds -= 1;
+      }
+  $scope.finish()
+  }
+
+  $scope.finish = function(){
+    if ($scope.countdown.seconds === 0 &&
+        $scope.countdown.minutes === 0 &&
+        $scope.countdown.hours === 0 ) {
+      clearInterval($scope.clear) 
+    }
+  }
+}) 
+
+
+
